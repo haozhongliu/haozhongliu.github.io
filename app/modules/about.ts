c129170ac579732881d437dedb0d18879bf8ec02
Kayl Liu <@example.com>
2026-09-21 13:07:19 -0500

    up

--- app/modules/about.ts
+++ app/modules/about.ts
@@ -31,7 +31,7 @@ export const aboutModule = {
     "Hi, I’m Haozhong Liu (Kayl, 刘皓中).",
     "I am a junior undergraduate in Computer Engineering, pursuing a dual bachelor’s degree at [the University of Illinois at Urbana-Champaign (UIUC)](https://illinois.edu/) and [Zhejiang University (ZJU)](https://www.zju.edu.cn/english/), [ZJUI](https://zjui.intl.zju.edu.cn/en) Class of 2028. ",
     "My interests include Human-Computer Interaction (HCI), Human-Agent Collaboration, Multi-Agent Systems, and Visualization. I am particularly interested in how interactive systems can support understanding, design, and creative thinking. I am currently working in the [VAI Group](https://zjuvai.cn/) in [the State Key Lab of CAD&CG](http://fit.zju.edu.cn/fitenglish/2019/0815/c36390a1484193/page.htm) at ZJU, advised by [Prof. Wei Chen](http://www.cad.zju.edu.cn/home/chenwei/) and [Prof. Wei Zhang](https://zwstart.github.io/). I am also open to collaborations and new research opportunities! I expect to graduate in Fall 2028 and plan to continue my research in the United States. Please feel free to reach out if you think there may be a good research fit!",
-    "I am from Qingdao, China. I enjoy traveling, poetry, and music.",
+    "I enjoy traveling, poetry, and music.",
     "Email: [hl123@illinois.edu](mailto:hl123@illinois.edu)",
     
   ],
